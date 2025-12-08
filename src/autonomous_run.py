bcd3d959a71c279c77732e3175808a5b50bfdfd7
Trevor Chartier <chartier.trevor@gmail.com>
2025-12-07 18:39:08 -0700

    Add Grad Cam

--- src/autonomous_run.py
+++ src/autonomous_run.py
@@ -3,6 +3,6 @@ from inference_driver import InferenceDriver
 from model import Model
 
 if __name__ == "__main__":
-    model_path = "/home/trevorchartier/Projects/ImitationLearningLaymo/models/005/model.keras"
+    model_path = "/home/trevorchartier/Projects/ImitationLearningLaymo/models/005/model.tflite"
     model = Model(model_path)
     run_driver(InferenceDriver, [model], print_inference_instructions)
